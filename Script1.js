2d50810c2fd7f7723bb117514ef0ba0fa9a3e586
User <user@email.com>
2020-09-14 07:23:57 -0700

    First push

---
+++ Script1.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+//alert("Hey there! Welcome to my page!");
+document.getElementById('email').onclick = click;
+
+function click() {
+    alert("Text/call: (425)503-7107 or Email: tylerfarhner@gmail.com");
+}
